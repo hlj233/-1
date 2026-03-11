a9817ab6a5351320808586ff578caffcfa07d8ca
wlfh <3222787639@qq.com>
2026-03-11 17:35:39 +0800

    Changes

---
+++ src/main/java/com/stu/hello_server/HelloServerApplication.java
@@ -0,0 +1,12 @@
+package com.stu.hello_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloServerApplication.class, args);
+    }
+}
